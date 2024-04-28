019282204015c9ca5d286efe170c0c070cd2088b
Levi Pires <leviapires16@gmail.com>
2024-04-27 22:28:05 -0300

    updates

--- src/attrs.ts
+++ src/attrs.ts
@@ -5,7 +5,7 @@ type Reserved<K extends string | number | symbol> = {
   [P in K]?: never;
 };
 
-export interface RenderAttrs {
+export interface Attributes {
   /**
    * Set (or override the default) `body` attributes.
    */
@@ -26,24 +26,64 @@ export interface RenderAttrs {
   fav_url?: string;
 }
 
-export type ProcessedRenderAttrs = Record<keyof RenderAttrs, string>;
+export type ProcessedRenderAttrs = Record<keyof Attributes, string>;
 
 export const $renderAttrsQueue: ProcessedRenderAttrs[] = [];
 
 function objToAttrs(obj: object) {
-  return Object.values(obj)
+  return Object.entries(obj)
     .map(([key, value]) => `${key}="${value}"`)
     .join(" ");
 }
 
-export function enqueueRenderAttrs(attrs: RenderAttrs, defaults: RenderAttrs) {
-  if (attrs.root_attrs) delete attrs.root_attrs.id;
-  if (defaults.root_attrs) delete defaults.root_attrs.id;
+export class RenderAttrs {
+  queue: Attributes[];
+  isCapturing = false;
+  currentCapture: Attributes[] | null = null;
 
-  $renderAttrsQueue.push({
-    body_attrs: objToAttrs(attrs.body_attrs || defaults.body_attrs || {}),
-    fav_url: attrs.fav_url || defaults.fav_url || "",
-    html_lang: attrs.html_lang || defaults.html_lang || "",
-    root_attrs: objToAttrs(attrs.root_attrs || defaults.root_attrs || {}),
-  });
+  constructor() {
+    this.queue = [];
+
+    this.queue.push = (...items) => {
+      if (this.isCapturing) {
+        this.currentCapture = (this.currentCapture || []).concat(items);
+      }
+
+      return Array.prototype.push.apply(this.queue, items);
+    };
+  }
+
+  startCapture() {
+    if (this.isCapturing) throw new Error("already capturing attrs");
+
+    this.isCapturing = true;
+  }
+
+  endCapture() {
+    if (!this.isCapturing) throw new Error("not capturing attrs");
+
+    this.isCapturing = false;
+
+    const result = this.currentCapture || [];
+
+    this.currentCapture = null;
+
+    return result;
+  }
+
+  merge(items: Attributes[]) {
+    return items.reduce((prev, curr) => ({ ...prev, ...curr }), {});
+  }
+
+  process(attrs: Attributes, defaults: Attributes): ProcessedRenderAttrs {
+    if (attrs.root_attrs) delete attrs.root_attrs.id;
+    if (defaults.root_attrs) delete defaults.root_attrs.id;
+
+    return {
+      body_attrs: objToAttrs(attrs.body_attrs || defaults.body_attrs || {}),
+      fav_url: attrs.fav_url || defaults.fav_url || "",
+      html_lang: attrs.html_lang || defaults.html_lang || "",
+      root_attrs: objToAttrs(attrs.root_attrs || defaults.root_attrs || {}),
+    };
+  }
 }
