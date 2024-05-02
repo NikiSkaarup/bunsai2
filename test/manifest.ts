c162a92109af4482296816173c34cc8743b56044
Levi Pires <mateus.amare@gmail.com>
2024-05-02 18:08:42 -0300

    updates

--- test/manifest.ts
+++ test/manifest.ts
@@ -1,7 +1,30 @@
 import { bunsai, createManifest } from "bunsai2/manifest";
-import "./src/ros.svelte";
-import "./src/test.svelte";
+import * as Ros from "./src/ros.svelte";
+import * as Test from "./src/test.svelte";
 
 const buns = await bunsai();
 
-if (buns) console.log(createManifest(buns));
+if (buns) {
+  const { manifest, render } = createManifest(buns);
+
+  Bun.serve({
+    fetch(req) {
+      const url = new URL(req.url);
+
+      const matched = manifest.get(url.pathname);
+
+      if (matched) return matched();
+
+      switch (url.pathname) {
+        case "/":
+          return render(Test, { req });
+        case "/ros":
+          return render(Ros, { req });
+        default:
+          return new Response("NOT FOUND", { status: 404 });
+      }
+    },
+  });
+
+  console.log("Serving");
+} else console.log("Nah...");
